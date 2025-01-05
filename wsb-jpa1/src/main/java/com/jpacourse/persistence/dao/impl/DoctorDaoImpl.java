e550af08aefc7095163cdf4cd61f939673d756ab
IgorKa <igor.karlik2973@gmail.com>
2025-01-05 17:13:37 +0100

    Add DoctorDao

--- wsb-jpa1/src/main/java/com/jpacourse/persistence/dao/impl/DoctorDaoImpl.java
+++ wsb-jpa1/src/main/java/com/jpacourse/persistence/dao/impl/DoctorDaoImpl.java
@@ -1,6 +1,7 @@
 package com.jpacourse.persistence.dao.impl;
 
+import com.jpacourse.persistence.dao.DoctorDao;
 import com.jpacourse.persistence.entity.DoctorEntity;
 
-public class DoctorDaoImpl extends AbstractDao<DoctorEntity, Long> {
+public class DoctorDaoImpl extends AbstractDao<DoctorEntity, Long> implements DoctorDao {
 }
