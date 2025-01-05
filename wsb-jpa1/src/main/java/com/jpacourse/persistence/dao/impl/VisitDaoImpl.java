790cd4e9c15fcd924d522c736f2d85645a75fafa
IgorKa <igor.karlik2973@gmail.com>
2025-01-05 17:00:04 +0100

    Update VisitDao

--- wsb-jpa1/src/main/java/com/jpacourse/persistence/dao/impl/VisitDaoImpl.java
+++ wsb-jpa1/src/main/java/com/jpacourse/persistence/dao/impl/VisitDaoImpl.java
@@ -1,6 +1,7 @@
 package com.jpacourse.persistence.dao.impl;
 
+import com.jpacourse.persistence.dao.VisitDao;
 import com.jpacourse.persistence.entity.VisitEntity;
 
-public class VisitDaoImpl extends AbstractDao<VisitEntity, Long> {
+public class VisitDaoImpl extends AbstractDao<VisitEntity, Long> implements VisitDao {
 }
